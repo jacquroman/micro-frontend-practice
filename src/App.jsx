4acf9af4d785e683186753e5d80b26cb7b7166cb
Jacqueline Roman <jacquelinedroman@gmail.com>
2023-02-24 14:33:08 -0800

    pdp folder
    pdp folder files

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Header from './Header';
-import Footer from './Footer';
 
 import "./index.scss";
+
+import Header from 'home/Header';
+import Footer from 'home/Footer';
 
 const App = () => (
   <div className="text-3xl mx-auto max-w-6xl">
     <Header />
     <div className="my-10">
-      Home Page Content
+      PDP Page Content
     </div>
     <Footer />
   </div>
